39b34a147da5111c03363f2cb79f463d878c5103
Joslink <jetsam00@gmail.com>
2019-11-29 19:18:22 -0400

    new version 0.0.1

---
+++ Scripts/ItemManager.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class ItemManager: MonoBehaviour
+{
+    Items[] currentInventory;
+    bool[] item_Uses;
+
+	public ItemManager()
+	{
+	}
+}
